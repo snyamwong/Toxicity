51df8d30835a07af739e69c4731425ab575de868
Tyler <tylercrosby@protonmail.com>
2019-07-14 22:18:18 -0400

    start

---
+++ DataCleaning.py
@@ -0,0 +1,17 @@
+import JSON
+
+
+def load_file(list_of_filenames):
+    for file in list_of_filenames:
+        json.load
+    # Filter out the non-target subreddits
+
+    # Clean data
+
+
+def target_subreddits(List_of_Subs):
+    pass
+
+
+def clean_data(filename):
+    pass
