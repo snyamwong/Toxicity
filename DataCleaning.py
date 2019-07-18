2531d584611dd6f48bca95a9fa9b6ac4a025fa1f
Tyler <tylercrosby@protonmail.com>
2019-07-17 21:44:11 -0400

    work

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,12 +1,16 @@
-import JSON
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
 
 
-def load_file(list_of_filenames):
-    for file in list_of_filenames:
-        json.load
-    # Filter out the non-target subreddits
+def load_files(data_path):
+    """
+    This method reads all files in the data folder. Should only contain JSON,
+    idk what will happen otherwise....
+    :return:
+    """
 
-    # Clean data
+    df = sqlContext.read.json(data_path)
+    return df
 
 
 def target_subreddits(List_of_Subs):
@@ -15,3 +19,16 @@ def target_subreddits(List_of_Subs):
 
 def clean_data(filename):
     pass
+
+
+def main():
+    data_path = "data/*"
+    raw_df = load_files(data_path)
+
+
+if __name__ == "__main__":
+    conf = SparkConf().setMaster("local[*]").setAppName("Toxicity-Cleaning")
+    sc = SparkContext(conf = conf)
+    sqlContext = SQLContext(sc)
+
+    main()
