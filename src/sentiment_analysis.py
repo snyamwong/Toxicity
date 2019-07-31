a52a8eb026f36886aeecb1de3053fa0acf5e896e
Tin Wong <wongt1@wit.edu>
2019-07-30 22:52:40 -0400

    used pandas to create a bar graph
    need to change colors, labels etc - those will be changed once results are returned

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -2,10 +2,11 @@
 Provides static helper functions for analyzing the sentiment of Reddit comments.
 """
 
+import matplotlib.pyplot as plt
 
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
+from pyspark.sql.functions import udf, mean
 from pyspark.sql.types import DoubleType
 from textblob import TextBlob
 
@@ -20,7 +21,7 @@ def create_spark():
 
 def get_comment_polarity(comment):
     """
-    Given a comment, return a tuple of comment, and its sentiment score.
+    Given a comment, return a tuple of comment, and its polarity score.
     """
     comment = TextBlob(comment)
 
@@ -29,11 +30,35 @@ def get_comment_polarity(comment):
 
 def get_comment_subjectivity(comment):
     """
-    Given a comment, return a tuple of comment, and its polarity score.
+    Given a comment, return a tuple of comment, and its subjectivity score.
     """
     comment = TextBlob(comment)
 
     return comment.sentiment.subjectivity
+
+
+def get_plot(dataframe):
+    """
+    Plots the dataframe
+
+    TODO: labels, legend and title all need to be manually changed. This is more for demo purposes.
+    """
+
+    plt.figure()
+
+    df_pandas = dataframe.toPandas()
+
+    df_plot = df_pandas.plot(
+        kind='bar', title='Average Polarity per Subreddit')
+
+    df_plot.set_xlabel('subreddit')
+
+    df_plot.set_ylabel('average polarity')
+
+    # TODO: need to manually change it here, should be dynamic
+    df_plot.legend(['reddit.com'])
+
+    plt.show()
 
 
 def main():
@@ -51,7 +76,17 @@ def main():
     data = data.withColumn('polarity', polarity_udf(data.body)) \
                .withColumn('subjectivity', subjectivity_udf(data.body))
 
-    data.show()
+    average_polarity = data.groupby('subreddit').agg(mean('polarity'))
+
+    get_plot(average_polarity)
+
+    #toxic_comments = data.orderBy('polarity')
+
+    #fake_comments = data.orderBy('subjectivity')
+
+    # pprint(toxic_comments.take(5))
+
+    # pprint(fake_comments.take(5))
 
 
 if __name__ == '__main__':
