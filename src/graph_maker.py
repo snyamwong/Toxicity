07b7e11ba54f2c4c8802ca7dcebf4d1be83c95b4
Tin Wong <wongt1@wit.edu>
2019-08-05 12:25:19 -0400

    some comestics for graphs changed

--- src/graph_maker.py
+++ src/graph_maker.py
@@ -18,31 +18,72 @@ def main():
     average_compound = pd.read_csv('../dump/RC_2016-11-compound.csv')
 
     polarity_plot = average_polarity.plot(
-        kind='bar', title='Average Polarity per Subreddit', x='subreddit', y='average')
+        kind='bar',
+        x='subreddit',
+        y='average',
+        legend=False)
 
-    polarity_plot.set_xlabel('subreddit')
+    polarity_plot.set_title('Average Polarity per Subreddit', fontsize=30)
 
-    polarity_plot.set_ylabel('average polarity')
+    polarity_plot.set_xlabel('subreddit', fontsize=28)
+
+    polarity_plot.set_ylabel('average polarity', fontsize=28)
 
     polarity_plot.legend(['subreddit'])
 
+    polarity_plot.set_xticklabels(polarity_plot.get_xticklabels(), rotation=0)
+
+    polarity_plot.get_legend().remove()
+
+    plt.xticks(size=24)
+
+    plt.yticks(size=24)
+
     subjectivity_plot = average_subjectivity.plot(
-        kind='bar', title='Average Subjectivity per Subreddit', x='subreddit', y='average')
+        kind='bar',
+        x='subreddit',
+        y='average',
+        legend=False)
 
-    subjectivity_plot.set_xlabel('subreddit')
+    subjectivity_plot.set_title(
+        'Average Subjectivity per Subreddit', fontsize=30)
 
-    subjectivity_plot.set_ylabel('average subjectivity')
+    subjectivity_plot.set_xlabel('subreddit', fontsize=28)
+
+    subjectivity_plot.set_ylabel('average subjectivity', fontsize=28)
 
     subjectivity_plot.legend(['subreddit'])
 
+    subjectivity_plot.set_xticklabels(
+        subjectivity_plot.get_xticklabels(), rotation=0)
+
+    subjectivity_plot.get_legend().remove()
+
+    plt.xticks(size=24)
+
+    plt.yticks(size=24)
+
     compound_plot = average_compound.plot(
-        kind='bar', title='Average Compound per Subreddit', x='subreddit', y='average')
+        kind='bar',
+        x='subreddit',
+        y='average',
+        legend=False)
 
-    compound_plot.set_xlabel('subreddit')
+    compound_plot.set_title('Average Compound per Subreddit', fontsize=30)
 
-    compound_plot.set_ylabel('average compound')
+    compound_plot.set_xlabel('subreddit', fontsize=28)
+
+    compound_plot.set_ylabel('average compound', fontsize=28)
 
     compound_plot.legend(['subreddit'])
+
+    compound_plot.set_xticklabels(compound_plot.get_xticklabels(), rotation=0)
+
+    compound_plot.get_legend().remove()
+
+    plt.xticks(size=24)
+
+    plt.yticks(size=24)
 
     plt.show()
 
